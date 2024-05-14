361754b6f0cc2cfacf015a18ef152c8a8af6efda
Timberfang <timberfang.code@pm.me>
2024-05-14 10:32:48 -0500

    refactor: Use Color instead of string for colors Commented out unused properties too

--- src/Models/FurChar.cs
+++ src/Models/FurChar.cs
@@ -5,24 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json;
 using System.IO;
+using System.Drawing;
 
 namespace FurCard.Models
 {
     public class Character
     {
-        public int ID { get; set; } = 0; // UUID per character
         public string Name { get; set; } = string.Empty;
         public string Species { get; set; } = string.Empty;
         public int Age { get; set; } = 0;
         public string Sex { get; set; } = string.Empty;
         public float Height { get; set; } = 0;
         public float Weight { get; set; } = 0;
-        public string EyeColor { get; set; } = string.Empty;
-        public List<string> BodyColors { get; set; } = new List<string>();
-        public List<string> PhysicalTraits { get; set; } = new List<string>();
-        public List<string> MentalTraits { get; set; } = new List<string>();
-        public List<string> Skills { get; set; } = new List<string>();
-        public List<string> Equipment { get; set; } = new List<string>();
+        public Color EyeColor { get; set; } = Color.Empty;
+        public List<Color> BodyColors { get; set; } = new List<Color>();
+        // public List<string> PhysicalTraits { get; set; } = new List<string>();
+        // public List<string> MentalTraits { get; set; } = new List<string>();
+        // public List<string> Skills { get; set; } = new List<string>();
+        // public List<string> Equipment { get; set; } = new List<string>();
         public string Background { get; set; } = string.Empty;
 
         public override string ToString()
@@ -32,14 +32,14 @@ namespace FurCard.Models
             Character.AppendLine($"{Name} ({Species}):");
             Character.AppendLine($"\tAge: {Age}, Sex: ${Sex}");
             Character.AppendLine($"\tHeight: {Height}m, Weight: ${Weight}kg");
-            Character.AppendLine($"\tEye Color: ${EyeColor}");
+            Character.AppendLine($"\tEye Color: ${EyeColor.Name}");
             Character.AppendLine($"\tBody Color(s):");
 
             // List all body colors, but don't have a comma for the last color.
             for (int i = 0; i <= BodyColors.Count; i++)
             {
-                if (i < BodyColors.Count) { Character.AppendLine($"\t\t{BodyColors[i]},"); }
-                else { Character.AppendLine($"\t\t{BodyColors[i]}"); }
+                if (i < BodyColors.Count) { Character.AppendLine($"\t\t{BodyColors[i].Name},"); }
+                else { Character.AppendLine($"\t\t{BodyColors[i].Name}"); }
             }
 
             Character.AppendLine($"\tBackground: {Background}");
@@ -51,7 +51,7 @@ namespace FurCard.Models
         public void Export(string OutPath, JsonSerializerOptions options)
         {
             // Check if OutPath is null or empty
-            if ( string.IsNullOrEmpty(OutPath) ) { throw new ArgumentOutOfRangeException("OutPath cannot be null or empty."); }
+            if (string.IsNullOrEmpty(OutPath)) { throw new ArgumentOutOfRangeException("OutPath cannot be null or empty."); }
 
             else {
                 // Create parent directory
@@ -66,5 +66,5 @@ namespace FurCard.Models
                 File.WriteAllText(OutPath, JsonString);
             }
         }
-    }
+    }   
 }
