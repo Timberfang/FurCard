2dc3749dba23888d657c708760cc590f0ae9fa0f
Timberfang <timberfang.code@pm.me>
2024-05-13 19:54:06 -0500

    feat: Create CardView

---
+++ src/Views/CardView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace FurCard.Views
+{
+    public partial class CardView : UserControl
+    {
+        public CardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
