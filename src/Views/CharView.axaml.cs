1be35263faa4a50433c1ce5ed0c5938a89fb64be
Timberfang <timberfang.code@pm.me>
2024-06-17 12:30:27 -0500

    feat: Create CharView

---
+++ src/Views/CharView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace FurCard.Views
+{
+    public partial class CharView : UserControl
+    {
+        public CharView()
+        {
+            InitializeComponent();
+        }
+    }
+}
