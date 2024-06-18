05dbeecc79124f125497f331cc068ae8a9b2aeea
Timberfang <timberfang.code@pm.me>
2024-06-18 11:21:53 -0500

    feat: Adjust layout depending on window width

--- src/Views/CharView.axaml.cs
+++ src/Views/CharView.axaml.cs
@@ -8,5 +8,21 @@ namespace FurCard.Views
         {
             InitializeComponent();
         }
+
+        // TODO: Find a better way to do this
+        protected override void OnSizeChanged(SizeChangedEventArgs e)
+        {
+            if (e.WidthChanged)
+            {
+                if (e.NewSize.Width < 800)
+                {
+                    MainView.Orientation = Avalonia.Layout.Orientation.Vertical;
+                }
+                else
+                {
+                    MainView.Orientation = Avalonia.Layout.Orientation.Horizontal;
+                }
+            }
+        }
     }
 }
